2c529a2cb683479939a06d6c277e685a5041ee2a
sideman05 <eligobagobby@gmail.com>
2026-02-21 12:50:40 +0300

    ports

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello from FreeLancePay backend!');
