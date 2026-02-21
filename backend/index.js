91e4181e1b6ae99e0fff31f95372a88ddb967d08
sideman05 <eligobagobby@gmail.com>
2026-02-21 12:31:05 +0300

    backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// Basic Express server entrypoint
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from FreeLancePay backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
